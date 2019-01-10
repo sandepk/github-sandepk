13e4e15ee10be383e58a1ba8c43152454f9eca26
sandepk <gupta231296@gmail.com>
2019-01-10 22:14:37 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,4 @@
+my_variable="<html><head><title>My first Code</title></head><body>Hello World<a href="">click me</a></body></html>"
+my_html_file=open("/users/sandepk/New folder/Desktop/python/my_html_file.html","w")
+my_html_file.write(my_variable)
+my m    
